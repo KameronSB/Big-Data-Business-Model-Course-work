7bb37fb9c0deaa01e0de295d50ec1d99d0eef943
Kameron Bains <kameronsbains@gmail.com>
2021-03-26 17:35:12 +0000

    First state, basic import of the data and a summary of the results

---
+++ CourseWork.R
@@ -0,0 +1,15 @@
+#Importig the data from CSV file
+
+
+#library(readxl)
+#creditcard <- read_excel("creditcard2.csv")
+
+#To import the data I right clicked on the creditcard2.csv in RStudio file
+#window and clicked import dataset
+
+
+summary(creditcard2)
+
+creditcard2$Time
+summary(creditcard2$Time)
+#Why is this not showing on GITHUB!!!!!!!!!!
